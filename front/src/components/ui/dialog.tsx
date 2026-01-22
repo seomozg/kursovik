248d0ddbe4832b2222b3b10353a0a7f0d78225c5
seomozg <seomozg@github.com>
2026-01-22 12:18:17 +0700

    Inline cn function into dialog.tsx component

--- front/src/components/ui/dialog.tsx
+++ front/src/components/ui/dialog.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-import { cn } from "@/lib/utils";
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 const Dialog = DialogPrimitive.Root;
 
