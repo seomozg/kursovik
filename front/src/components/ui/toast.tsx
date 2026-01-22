74dacf0f98b29f874e0917a32d6b1aa9e6ee288c
seomozg <seomozg@github.com>
2026-01-22 11:59:53 +0700

    Inline cn function into toast.tsx component to complete the fix

--- front/src/components/ui/toast.tsx
+++ front/src/components/ui/toast.tsx
@@ -2,8 +2,12 @@ import * as React from "react";
 import * as ToastPrimitives from "@radix-ui/react-toast";
 import { cva, type VariantProps } from "class-variance-authority";
 import { X } from "lucide-react";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-import { cn } from "@/lib/utils";
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 const ToastProvider = ToastPrimitives.Provider;
 
