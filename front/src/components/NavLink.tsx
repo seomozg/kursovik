8a2ce48d60e6fc60898c05c2775baeb376e06d79
seomozg <seomozg@github.com>
2026-01-22 12:04:23 +0700

    Inline cn function into NavLink.tsx component to complete the fix

--- front/src/components/NavLink.tsx
+++ front/src/components/NavLink.tsx
@@ -1,6 +1,11 @@
 import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
 import { forwardRef } from "react";
-import { cn } from "@/lib/utils";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
   className?: string;
